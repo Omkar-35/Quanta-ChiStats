00c925288cbf6f3912a0ee30bda1fe22190cb609
Palak Chandak <palakchandak1805@gmail.com>
2025-05-28 13:59:17 +0530

    database updated

--- backend/database.py
+++ backend/database.py
@@ -3,8 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from fastapi import Depends
 
-call_db_URL = "postgresql://postgres:Omkar%4016@localhost/options_ce_db"
-put_db_URL = "postgresql://postgres:Omkar%4016@localhost/options_pe_db"
+call_db_URL = "postgresql://postgres:postgres@localhost/options_ce_db"
+put_db_URL = "postgresql://postgres:postgres@localhost/options_pe_db"
 
 call_engine = create_engine(call_db_URL)
 put_engine = create_engine(put_db_URL)
